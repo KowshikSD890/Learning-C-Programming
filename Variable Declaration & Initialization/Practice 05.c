acc27ed9b8e83a0223683bc908a68053a40256e1
Kowshik <xhkaushik890@gmail.com>
2024-09-23 21:36:08 +0600

    Add files via upload

---
+++ Variable Declaration & Initialization/Practice 05.c
@@ -0,0 +1,14 @@
+/* Write a Program That Take Three Integer variable a,b,and c assign values to
+to the variables a=10, b=20, c=30, now print the some of this three variable*/
+
+#include<stdio.h>
+int main()
+{
+    int a=10, b=20, c=30, sum;
+
+    sum= a+b+c;
+    printf("The Sum Of a, b, c : %d \n", sum);
+
+
+    return 0;
+}
