08bbbe324d4d33ad758c47fbedd63408920decd8
Kowshik <xhkaushik890@gmail.com>
2024-09-23 22:01:57 +0600

    Add files via upload

---
+++ Variable Declaration & Initialization/Practice 06.c
@@ -0,0 +1,13 @@
+// ASCII Value
+
+#include<stdio.h>
+int main()
+{
+
+    printf("The ASCII Value Of A is : %d \n",'A' );
+
+    printf("The ASCII Value Of @ is : %d \n", '@');
+
+
+    return 0;
+}
