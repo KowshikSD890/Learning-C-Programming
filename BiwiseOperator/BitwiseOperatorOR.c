6dfc32556a8ff9cca1acb3b45a7f31412c86e6c5
Kowshik <xhkaushik890@gmail.com>
2025-05-23 18:33:32 +0700

    Update and rename BitwiseOperator2.c to BitwiseOperatorOR.c

--- BiwiseOperator/BitwiseOperatorOR.c
+++ BiwiseOperator/BitwiseOperatorOR.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    /*
-    101
-    110
+     /*
+    101 ---5
+    110 ---6
     ____
-
+    1 1 1---7
 
     */
 int x,y;
 printf("enter the value of x & y :");
 scanf("%d %d",&x,&y);
 
-int result = x & y ; // bitwise and
+int result = x | y ; // bitwise or
 printf("result is : %d", result);
 
 
 
 return 0;
 }
-
